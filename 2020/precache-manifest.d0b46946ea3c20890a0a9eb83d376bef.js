72d2ea082c5e1afbd584632853084e493dfbd3e3
Prabu Mani <prabube.mani@gmail.com>
2019-12-27 15:50:27 +0530

    2020 path update

---
+++ 2020/precache-manifest.d0b46946ea3c20890a0a9eb83d376bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a979bdc4bfc13c54ec08a20d3922073b",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "f8b31916f703cdd29265",
+    "url": "/2020/static/css/main.c3bb0adf.chunk.css"
+  },
+  {
+    "revision": "9c824c6f7be0be630229",
+    "url": "/2020/static/js/2.9cfbfee9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/2020/static/js/2.9cfbfee9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8b31916f703cdd29265",
+    "url": "/2020/static/js/main.b12d41f3.chunk.js"
+  },
+  {
+    "revision": "a962a3aed81fc1f526f1",
+    "url": "/2020/static/js/runtime-main.6ec5c60c.js"
+  }
+]);
