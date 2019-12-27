7cbda3997b09c6715ac46357d5f72d285e21125b
Prabu Mani <prabube.mani@gmail.com>
2019-12-27 15:39:52 +0530

    2020 Uploaded to Live

---
+++ 2020/precache-manifest.16635e9789db442994cd7704106affe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760cc0f678f9c072fa8626a6e3946600",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "51901648167c3d75c767",
+    "url": "/2020/static/css/main.c3bb0adf.chunk.css"
+  },
+  {
+    "revision": "9c824c6f7be0be630229",
+    "url": "/2020/static/js/2.9cfbfee9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/2020/static/js/2.9cfbfee9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51901648167c3d75c767",
+    "url": "/2020/static/js/main.1bb4f976.chunk.js"
+  },
+  {
+    "revision": "a962a3aed81fc1f526f1",
+    "url": "/2020/static/js/runtime-main.6ec5c60c.js"
+  }
+]);
